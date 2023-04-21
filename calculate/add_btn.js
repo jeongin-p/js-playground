d7fb2f953fb240b04e72832e78a18bc75324e988
jeongin-p <jeongin.park@dsrvlabs.com>
2023-04-21 17:49:37 +0900

    공부중이예요...객체를 이용한 계산기 작업 집에서 마저 합시다

--- calculate/add_btn.js
+++ calculate/add_btn.js
@@ -1,1 +1,18 @@
 console.log("botton js");
+var test = document.getElementById("test");
+var menu = document.createElement("ul");
+var item1 = document.createElement("li");
+item1.innerHTML = "치킨";
+menu.appendChild(item1);
+var item2 = document.createElement("li");
+item2.innerHTML = "짜장면";
+menu.appendChild(item2);
+test.appendChild(menu);
+
+var readInput = function () {
+  var input = document.getElementById("input1");
+  console.log(input.value);
+};
+
+var btn1 = document.getElementById("btn1");
+btn1.onclick = readInput;
